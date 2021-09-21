7d2e1c095116dc784c0cdc545c16222f0e112480
roger chat <mattrogerpelosi@gmail.com>
2021-09-21 17:20:23 -0400

    commit tree fd42d2762f63f8c0f7f4e8fe51ea09cad6ebed8d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
 
+    def name=(name) #setter
+        @name = name
+    end
+
+    def name #getter
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed 
+        @breed
+    end 
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
 
+    def name=(name) #setter
+        @name = name
+    end
+
+    def name #getter
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job 
+        @job
+    end 
+
 end
